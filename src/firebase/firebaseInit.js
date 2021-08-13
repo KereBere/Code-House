8b490d68a1bf37832bce63592d5dea9ee511c7fc
KereBere <86746444+KereBere@users.noreply.github.com>
2021-08-13 14:58:40 +0300

    Update firebaseInit.js

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,18 +1,2 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore'; 
-
-
-var firebaseConfig = {
-    apiKey: "AIzaSyDWCbZDLUMRvnjvIgjgL_qEiNkGy4JCnVw",
-    authDomain: "fireblogs-7c7cb.firebaseapp.com",
-    projectId: "fireblogs-7c7cb",
-    storageBucket: "fireblogs-7c7cb.appspot.com",
-    messagingSenderId: "433572041395",
-    appId: "1:433572041395:web:4dc5e2dac62a43d0ead6c6"
-  };
-
-  const firebaseApp = firebase.initializeApp(firebaseConfig); 
-  const timeStamp = firebase.firestore.FieldValue.serverTimestamp;
-
-  export {timeStamp};
-  export default firebaseApp.firestore();
+//
+//Firebase settings and API here
